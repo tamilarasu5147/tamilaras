14ad75fb4fbbb9e40a42b1d47ada9538cfadd992
tamilarasu5147 <157339349+tamilarasu5147@users.noreply.github.com>
2024-04-29 15:38:51 +0530

    APR-13

---
+++ Apr 13-Day 20/Triangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int q,w=0;
+    scanf("%d ",&q);
+    for(int e=0;e<q;e++){
+        for(int t=e;t<=e;t++){
+            printf("%d ",w++);
+        }
+        printf("\n");
+    }
+}
+
+o/p:
+/*
+1 
+2 3 
+4 5 6 
+7 8 9 10
+*/
